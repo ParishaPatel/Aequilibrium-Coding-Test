5d1b658979a74ce359267cfc58b6c44a1114a283
AlexSiminiuc <alex@alexsiminiuc.com>
2019-06-17 07:57:25 -0700

    Update Driver.java

--- AequSwagLab/src/main/java/codingTest/AequSwagLab/Browser/Driver.java
+++ AequSwagLab/src/main/java/codingTest/AequSwagLab/Browser/Driver.java
@@ -17,7 +17,11 @@ import codingTest.AequSwagLab.Report.LogStatus;
 
 public class Driver extends ReadPropertyFile
 {
+	//do not use a static driver
+	//do not use null
 	public static WebDriver driver=null;
+	
+	//getDriver() is way too long, consider breaking it in multiple smaller methods
 	
 	// Initializes browser based on inputs from properties file
 	public static void getDriver ()
@@ -25,13 +29,19 @@ public class Driver extends ReadPropertyFile
 		String browser=ReadPropertyFile.get("Browser");
 		String headless=ReadPropertyFile.get("HeadlessMode");
 		String imageDisable=ReadPropertyFile.get("DisableImage");
+		
+		//the second condition is useless, the first one is enough
 		if(browser.equalsIgnoreCase("chrome")|| browser.toUpperCase().contains("CHROME"))
 		{
 			try{
 				
 				System.setProperty("webdriver.chrome.driver",OsPath.getPath(browser));
 				ChromeOptions options=new ChromeOptions();
+				
+				//no need for this argument, the session will be incognito by default
 				options.addArguments("--incognito");
+				
+				//dont do this, it is not useful
 				if(imageDisable.equalsIgnoreCase("yes"))
 				{
 					new DisableImage().disableImg(options);
@@ -49,9 +59,12 @@ public class Driver extends ReadPropertyFile
 			}
 			catch (Exception e)
 			{
+				//if there is an exception, the execution should stop; logging info to console is not what you should do here
 				e.printStackTrace();
 			}
 		}
+		
+		//same comments as above
 		else if (browser.equalsIgnoreCase("FF")|| browser.toUpperCase().contains("FIRE")) 
 		{
 			try
@@ -82,10 +95,16 @@ public class Driver extends ReadPropertyFile
 			}
 		}
 		driver.manage().window().maximize();
+		
+		//implicit waits are a bad practice, if you need synchronization, use explicit waits and expected conditions
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(ReadPropertyFile.get("url"));
+		
+		//each session is clean, with no cookies persisted, no need to delete cookies
 		driver.manage().deleteAllCookies();
 	}
+	
+	//why the need to explain what it does? the method name and code are clear
 	
 	//quits browser
 	public static void quit()
